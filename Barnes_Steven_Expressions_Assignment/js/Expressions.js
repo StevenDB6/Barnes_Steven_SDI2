4ce58f0d89f60aeacfaffa102ccf025d98af2b7f
Steven <stevendbarnes@comcast.net>
2015-06-10 22:01:17 -0500

    Asked the user the weight.
    Asked the user the weight of the plane and the weight of the luggage.

--- Barnes_Steven_Expressions_Assignment/js/Expressions.js
+++ Barnes_Steven_Expressions_Assignment/js/Expressions.js
@@ -10,10 +10,26 @@ console.log("Test");
 
 // Ask the user what is their name
 var name = prompt("What is your name")
+
 // console.log the name to check it
 console.log (name)
 
 // Alert the user welcome
-alert(" Welcome " + name+" lets see how much your airplane weighs");
+alert(" Welcome " + name+" Let's see how much your airplane weighs");
 
-//Ask the user 
+//Ask the user what is the weight of the plane
+var planeWeight = prompt (" How much does your plane weigh?");
+console.log (planeWeight);
+
+//Calculate the weight of the luggage
+var luggageWeight = prompt("How much does the luggage weigh?");
+console.log (luggageWeight);
+
+// Calculate the total weight of the plane
+//Casting prompted values as numbers
+
+totalWeight = Number(planeWeight) + Number(luggageWeight);
+
+//Alert the user the total weight
+alert(name+" the plane weighs " +totalWeight+" pounds.");
+console.log (totalWeight);
