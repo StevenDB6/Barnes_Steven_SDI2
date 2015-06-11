8a5b9615e2729ad09b094c1f049c8eb143b78139
Steven <stevendbarnes@comcast.net>
2015-06-10 21:35:38 -0500

    Asked the user his or her name.
    Asked the user his or her name and also told the user “Welcome”

--- Barnes_Steven_Expressions_Assignment/js/Expressions.js
+++ Barnes_Steven_Expressions_Assignment/js/Expressions.js
@@ -6,3 +6,14 @@ Section 02
 //Test to see if connected
 console.log("Test");
 
+// Create a weight calculator
+
+// Ask the user what is their name
+var name = prompt("What is your name")
+// console.log the name to check it
+console.log (name)
+
+// Alert the user welcome
+alert(" Welcome " + name+" lets see how much your airplane weighs");
+
+//Ask the user 
