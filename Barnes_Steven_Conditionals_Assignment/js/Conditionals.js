7c5a3ce0c631831fe738be9958035c50089ed956
Steven <stevendbarnes@comcast.net>
2015-06-18 20:48:07 -0500

    Created a conditional
    Created a conditional to tell if a number is greater than or less than

--- Barnes_Steven_Conditionals_Assignment/js/Conditionals.js
+++ Barnes_Steven_Conditionals_Assignment/js/Conditionals.js
@@ -10,6 +10,26 @@
 
  //create a conditional
 
- // Ask the user to pick a random number
- var randomNumber = prompt(" Pick a number between 1 and 10")
- console.log (randomNumber)
+ // Ask the user to pick a even number
+ var evenNumber = prompt(" Pick a even number between 1 and 20");
+ console.log (evenNumber)
+
+ // ask the user to choose an odd number
+ var oddNumber = prompt(" Pick a odd number between 1 and 20 ");
+ console.log(oddNumber)
+
+ if( Number(evenNumber) < Number(oddNumber)){
+  //code will run if true
+  console.log(evenNumber+" is less than " +oddNumber);
+ } else{
+  console.log( evenNumber+ " is not less then " +oddNumber);
+ }
+
+ if( Number(evenNumber) > Number(oddNumber)){
+  //code will run if true
+  console.log(evenNumber+" is greater than " +oddNumber);
+ } else{
+  console.log( evenNumber+ " is not greater then " +oddNumber);
+ }
+
+ 
