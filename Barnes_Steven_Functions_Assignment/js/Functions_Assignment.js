342289d0493dadeaea2553b1e6087e9880b21b25
Steven <stevendbarnes@comcast.net>
2015-06-25 21:08:22 -0500

    Created two while loops.
    Asked the user what is the length and width.

--- Barnes_Steven_Functions_Assignment/js/Functions_Assignment.js
+++ Barnes_Steven_Functions_Assignment/js/Functions_Assignment.js
@@ -7,3 +7,26 @@
 
 // Testing to see if connected
 console.log("Test");
+
+// Create a file that will find the perimeter of a rectangle
+
+var rectanglePerimeter= prompt(" Lets find the perimeter of a rectangle.");
+
+// Ask user what is the length of the rectangle
+var rectangleLength= prompt(" What is the length?")
+
+//validate the length of the rectangle
+while(rectangleLength==="" || isNaN(rectangleLength)){
+        rectangleLength = prompt(" Please do not leave blank and only use numbers." +
+            "\nWhat is the length?");
+    }
+
+// Ask the user what is the width of the rectangle
+var rectangleWidth= prompt(" What is the width ");
+
+//validate the width of the rectangle
+while(rectangleWidth==="" || isNaN(rectangleWidth)){
+    rectangleWidth = prompt(" Please do not leave blank and only use numbers.\n" +
+        " What is the width?")
+}
+
